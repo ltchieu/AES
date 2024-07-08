9fb5b0460d5b3be4a48cb2162c7021fdd600d121
Công Hiếu <165462821+ltchieu@users.noreply.github.com>
2024-07-08 18:55:08 +0700

    Add files via upload

---
+++ aes.h
@@ -0,0 +1,48 @@
+﻿#pragma
+
+#ifndef AES_H
+#define AES_H
+
+#include<stdio.h>
+#include<stdint.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAXCol 100
+#define Nb 4
+
+extern uint8_t s_box[256];
+extern uint8_t Rcon[11];
+
+		// khai bao ham
+
+	// Cac ham ma hoa
+void AddAroundKey(uint8_t Key[], uint8_t Result[], int r);
+void SubBytes(uint8_t Result[]);
+void ShiftRows(uint8_t Result[]);
+void ShiftRow(uint8_t Result[], int row, int shift);
+void MixColumns(uint8_t Result[], uint8_t Mix_box[]);
+void aes_cipher(uint8_t Ptext[], uint8_t key[], uint8_t CipherText[]);
+
+
+	//Cac ham sinh khoa vong
+uint8_t rot_word(uint8_t* w);
+void sub_word(uint8_t w[], uint8_t s_box[]);
+void KeyExpansion(uint8_t* key, uint8_t* w, uint8_t* Rcon, uint8_t s_box[]);
+
+
+	// Mot so ham phu tro
+int Nhan2(uint8_t w);
+int Nhan3(uint8_t w);
+int nhapMangThapLucPhan(uint8_t arr[]);
+
+
+// Cac ham Giai ma
+void des_cipher(uint8_t Ptext[], uint8_t w[], uint8_t CipherText[]);
+void invShiftRows(uint8_t Result[]);
+void invShiftRow(uint8_t Result[], int row, int shift);
+void invSubBytes(uint8_t Result[]);
+void invMixColumns(uint8_t Result[], uint8_t invMix_box[]);
+#endif 
+
+
---
+++ main.cpp
@@ -0,0 +1,79 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "aes.h"
+
+void main() {
+
+	uint8_t key[16];
+	uint8_t Ptext[16];
+	uint8_t CText[16];
+	uint8_t* w = new uint8_t; //expand key
+	int choice;
+	
+	printf("\n");
+	printf("---------------MENU---------------\n");
+	printf("|1. Ma Hoa                      |\n");
+	printf("|2. Giai Ma                     |\n");
+	printf("|0. De thoat chuong trinh       |\n");
+	printf("---------------------------------\n");
+	printf("Moi nhap lua chon: ");
+	scanf("%d", &choice);
+
+
+	try
+	{
+		switch (choice)
+		{
+		case 1:
+			printf("Nhap Khoa k: ");
+			nhapMangThapLucPhan(key);
+			printf("\nNhap PlainText: ");
+			nhapMangThapLucPhan(Ptext);
+
+			KeyExpansion(key, w, Rcon, s_box); // key schedule
+
+			printf("\nPlaintext message:\n");
+			for (int i = 0; i < 4; i++) {
+				printf("%02x %02x %02x %02x ", Ptext[4 * i + 0], Ptext[4 * i + 1], Ptext[4 * i + 2], Ptext[4 * i + 3]);
+			}
+			printf("\n");
+
+			aes_cipher(Ptext, w, CText);
+
+			printf("Ciphered message:\n");
+			for (int i = 0; i < 4; i++) {
+				printf("%02x %02x %02x %02x ", CText[4 * i + 0], CText[4 * i + 1], CText[4 * i + 2], CText[4 * i + 3]);
+			}
+			printf("\n");
+			break;
+
+		case 2:
+			printf("Nhap Khoa k: ");
+			nhapMangThapLucPhan(key);
+			printf("\nNhap CipherText: ");
+			nhapMangThapLucPhan(CText);
+
+			KeyExpansion(key, w, Rcon, s_box); // key schedule
+
+			printf("Ciphered message:\n");
+			for (int i = 0; i < 4; i++) {
+				printf("%02x %02x %02x %02x ", CText[4 * i + 0], CText[4 * i + 1], CText[4 * i + 2], CText[4 * i + 3]);
+			}
+			printf("\n");
+			des_cipher(Ptext, w, CText);
+
+			printf("Plaintext message:\n");
+			for (int i = 0; i < 4; i++) {
+				printf("%02x %02x %02x %02x ", Ptext[4 * i + 0], Ptext[4 * i + 1], Ptext[4 * i + 2], Ptext[4 * i + 3]);
+			}
+			printf("\n");
+			break;
+		default:
+			break;
+		}
+	}
+	catch (int errCode)
+	{
+		if (errCode == 101)
+			printf("Do dai chuoi hex khong dung. Vui long nhap chuoi co 32 ky tu.\n");
+	}	
+}
